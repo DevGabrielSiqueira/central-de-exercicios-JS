f7b3f35589a965777879b2ce5108a59aa8e72dc3
Gabriel <gabriel_ssiqueira@hotmail.com>
2025-04-03 21:51:12 -0300

    Exercicios

---
+++ condicionais.js
@@ -0,0 +1,54 @@
+/* let numero = prompt ("Digite um numero:");
+
+if (numero > 0) {
+
+    console.log("Positivo.");
+} else {
+    
+    if (numero < 0){
+
+        console.log("Negativo.");   
+    }else {
+
+        consolelog("É zero.")
+    }
+} */
+
+    /* let numero = prompt ("Digite um número para verificar se é par ou ímpar: ")
+
+    if ( numero %2==0){
+
+        console.log ("O número é par")
+    }else {
+        console.log("o número é ímpar")
+    } */
+
+
+    /* let idade = prompt ("Digite sua idade")
+
+    if (idade >= 18){
+
+        console.log ("Maior de idade")
+
+    } else{
+
+        console.log("Menor de idade")
+    } */
+
+/* 
+        let idade = prompt ("Digite sua idade")
+
+        if (idade >=0 && idade<=12){
+
+            console.log ("Crianca")
+
+        } else if (idade >=13 && idade <=17){
+
+            console.log("Adolescente")
+
+        }else{
+
+            console.log ("Adulto")
+        } */
+
+            let nota = prompt ("Digite sua nota")
---
+++ scripts.js
@@ -0,0 +1,34 @@
+//alert()
+
+//console.log("Hello World no console");
+
+let nome = "Gabriel";
+const idade = 20;
+var cidade = "São Paulo";
+
+console.log(nome);
+console.log(idade); 
+console.log(cidade);
+
+console.log("Meu nome é: " + nome); // Concatenacao
+console.log(`Meu nome é: ${nome}`); // interpolacão
+
+console.log(`Meu nome é: ${nome}`);
+console.log(`Minha idade é: ${idade}`);
+console.log(`Minha cidade é: ${cidade}`);
+console.log(`Tenho ${idade} anos`);
+
+let texto = "Meu texto"; //string
+let numero = 20; //number
+
+console.log (texto);
+console.log (numero);
+
+console.log(typeof idade);   
+
+let n1 = 10;
+let n2 = 10;
+
+let soma = n1 + n2
+
+console.log(soma);
