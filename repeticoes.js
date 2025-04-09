bc0983acb81a1d718876c53396af5cbeae5ce125
Gabriel <gabriel_ssiqueira@hotmail.com>
2025-04-08 21:16:00 -0300

    exercicios repeticoes

--- repeticoes.js
+++ repeticoes.js
@@ -18,3 +18,34 @@ function basic01() {
         alert(contador);
     }
 }
+
+function exemplotabuada(){
+    let numero = (prompt("Digite um número para ver a tabuada:"));
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} x ${i} = ${numero * i}`);
+}
+}
+
+function exemploN(){
+    let n = (prompt("Digite um número N:"));
+let soma = 0;
+
+for (let i = 1; i <= n; i++) {
+  soma += i;
+}
+
+console.log(`A soma dos primeiros ${n} números naturais é: ${soma}`);
+}
+
+
+function exercicioint01(){
+    let numero = (prompt("Digite o número"));
+    
+     for (let i = 2; i <= 50; i+=2) {
+        console.log(i);
+       
+            
+        }
+    }
+exercicioint01();
